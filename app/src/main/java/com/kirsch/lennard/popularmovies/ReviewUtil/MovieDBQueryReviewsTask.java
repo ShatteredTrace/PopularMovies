7657c2abc237ef07b7c5547a0c6625f25c43ce50
ShatteredTrace <lk03@arcor.de>
2018-04-08 21:04:19 +0200

    Added MovieDBQueryReviewTask.java

---
+++ app/src/main/java/com/kirsch/lennard/popularmovies/ReviewUtil/MovieDBQueryReviewsTask.java
@@ -0,0 +1,33 @@
+package com.kirsch.lennard.popularmovies.ReviewUtil;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import com.kirsch.lennard.popularmovies.AsyncTaskInterface;
+
+import java.net.URL;
+
+public class MovieDBQueryReviewsTask extends AsyncTask<URL, Void, String> {
+    private AsyncTaskInterface<String> listener;
+
+    private Context context;
+    private int movieID;
+
+    public MovieDBQueryReviewsTask(Context context, int movieID, AsyncTaskInterface<String> listener){
+        this.context = context;
+        this.listener = listener;
+        this.movieID = movieID;
+    }
+
+    @Override
+    protected String doInBackground(URL... urls) {
+        String results = null;
+
+        return results;
+    }
+
+    @Override
+    protected void onPostExecute(String results) {
+        listener.onTaskComplete(results);
+    }
+}
